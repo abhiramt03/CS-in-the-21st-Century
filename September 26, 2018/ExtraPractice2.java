4ea771eab9d2a22362e165abe017c45cfa132034
Abhiram <abhiramt03@gmail.com>
2018-09-26 21:15:31 -0400

    ExtraPractice3.java

--- September 26, 2018/ExtraPractice2.java
+++ September 26, 2018/ExtraPractice2.java
@@ -1,14 +1,13 @@
 //Abhiramakrishnan Thiyagarajan
 
-	public class ExtraPractice2 {
+public class ExtraPractice2 {
 
-	public static void main(String[]args){
+public static void main(String[]args){
 
-		System.out.print("* * * * *\");
-		System.out.print("*\t""\*\n");
-		System.out.print("*\t"\n");
-		System.out.print("*\t"""\n");
-		System.out.print("* * * * *\");
-
+		System.out.println("* \t* \t* \t* \t*");
+		System.out.println("*\t \" \t\"\t*");
+		System.out.println("*     \"  \t*");
+		System.out.println("*  \"\t\"\t\" \t*");
+		System.out.println("* \t*\t*\t*\t*");
 	}
 }
---
+++ September 26, 2018/ExtraPractice3.java
@@ -0,0 +1,15 @@
+//Abhiramakrishnan Thiyagarajan
+
+public class ExtraPractice3 {
+
+	public static void main(String [] args) {
+
+		System.out.print("           *\n");
+		System.out.print("        *    *\n");
+		System.out.print("      *        *\n");
+		System.out.print("    *            *\n");
+		System.out.print("  *                *\n");
+		System.out.print("*  *  *  *  *  *  *  *\n");
+
+	}
+}
